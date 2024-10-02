e7dcdf04e51d548a4b25551eb44ec3b71dcdb517
crop <crop_tech@proton.me>
2024-10-02 21:54:58 +0200

    minor fixes

--- examples/example-array/build.rs
+++ examples/example-array/build.rs
@@ -2,7 +2,5 @@ use faust_build::FaustBuilder;
 
 fn main() {
     println!("cargo:rerun-if-changed=dsp");
-    FaustBuilder::new("dsp/volume.dsp", "src/dsp.rs")
-        // .set_use_double(true)
-        .build();
+    FaustBuilder::new("dsp/volume.dsp", "src/dsp.rs").build();
 }
