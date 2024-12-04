392e01d4edc0b8af06b0f1ca4f93603a16140831
crop <crop_tech@proton.me>
2024-12-04 16:11:41 +0100

    use local faust

--- examples/example-f64/build.rs
+++ examples/example-f64/build.rs
@@ -2,7 +2,11 @@ use faust_build::FaustBuilder;
 
 fn main() {
     println!("cargo:rerun-if-changed=dsp");
-    let b = FaustBuilder::new("dsp/volume.dsp", "src/dsp.rs").set_use_double(true);
+    let b = FaustBuilder::new("dsp/volume.dsp", "src/dsp.rs")
+        .set_faust_path("/home/olaf/projects/rust/faust4rust/faust-test/faust/build/bin/faust")
+        .faust_arg("-I")
+        .faust_arg("/home/olaf/projects/rust/faust4rust/faust-test/faust/libraries/")
+        .set_use_double(true);
     b.build();
     b.build_xml();
 }
