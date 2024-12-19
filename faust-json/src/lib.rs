cead7b812584d34b3326e6553c1e6284731740c1
crop <crop_tech@proton.me>
2024-12-19 16:50:52 +0100

    generate enums from json

--- faust-json/src/lib.rs
+++ faust-json/src/lib.rs
@@ -1,3 +1,5 @@
+use proc_macro2::TokenStream;
+use quote::{format_ident, quote, ToTokens};
 use serde::{Deserialize, Deserializer};
 
 #[derive(Debug, Deserialize)]
@@ -154,4 +156,183 @@ pub enum LayoutItem {
         #[serde(default)]
         meta: Vec<Meta>,
     },
+}
+
+pub struct ParamInfo {
+    is_active: bool,
+    shortname: String,
+    varname: String,
+}
+
+pub trait GetParmInfo {
+    fn get_param_info(&self) -> Vec<ParamInfo>;
+}
+
+impl GetParmInfo for Faust {
+    fn get_param_info(&self) -> Vec<ParamInfo> {
+        self.ui
+            .iter()
+            .flat_map(|items| items.get_param_info())
+            .collect()
+    }
+}
+
+impl GetParmInfo for LayoutItem {
+    fn get_param_info(&self) -> Vec<ParamInfo> {
+        match self {
+            LayoutItem::TGroup { items, .. } => items
+                .iter()
+                .flat_map(|items| items.get_param_info())
+                .collect(),
+            LayoutItem::VGroup { items, .. } => items
+                .iter()
+                .flat_map(|items| items.get_param_info())
+                .collect(),
+            LayoutItem::HGroup { items, .. } => items
+                .iter()
+                .flat_map(|items| items.get_param_info())
+                .collect(),
+            LayoutItem::VSlider {
+                shortname, varname, ..
+            } => vec![ParamInfo {
+                is_active: true,
+                shortname: shortname.to_string(),
+                varname: varname.to_string(),
+            }],
+            LayoutItem::HSlider {
+                shortname, varname, ..
+            } => vec![ParamInfo {
+                is_active: true,
+                shortname: shortname.to_string(),
+                varname: varname.to_string(),
+            }],
+            LayoutItem::NEntry {
+                shortname, varname, ..
+            } => vec![ParamInfo {
+                is_active: true,
+                shortname: shortname.to_string(),
+                varname: varname.to_string(),
+            }],
+            LayoutItem::Button {
+                shortname, varname, ..
+            } => vec![ParamInfo {
+                is_active: true,
+                shortname: shortname.to_string(),
+                varname: varname.to_string(),
+            }],
+            LayoutItem::CheckBox {
+                shortname, varname, ..
+            } => vec![ParamInfo {
+                is_active: true,
+                shortname: shortname.to_string(),
+                varname: varname.to_string(),
+            }],
+            LayoutItem::VBarGraph {
+                shortname, varname, ..
+            } => vec![ParamInfo {
+                is_active: false,
+                shortname: shortname.to_string(),
+                varname: varname.to_string(),
+            }],
+            LayoutItem::HBarGraph {
+                shortname, varname, ..
+            } => vec![ParamInfo {
+                is_active: false,
+                shortname: shortname.to_string(),
+                varname: varname.to_string(),
+            }],
+            LayoutItem::Soundfile {
+                address, varname, ..
+            } => {
+                vec![ParamInfo {
+                    is_active: false,
+                    shortname: address.to_string(),
+                    varname: varname.to_string(),
+                }]
+            }
+        }
+    }
+}
+
+fn create_qualified_enum(infos: &[&ParamInfo], quality: &str) -> TokenStream {
+    let enumname = format_ident!("UI{}Shortname", quality);
+    let i: Vec<TokenStream> = infos
+        .iter()
+        .map(|param_info| format_ident!("{}", param_info.shortname).to_token_stream())
+        .collect();
+    quote! { pub enum #enumname {
+            #(#i ),*
+        }
+    }
+}
+
+fn create_active_impl(infos: &[&ParamInfo], dsp_name: &str) -> TokenStream {
+    let enumname = format_ident!("UIActiveShortname");
+    let dsp_name = format_ident!("{dsp_name}");
+    let matches: Vec<TokenStream> = infos
+        .iter()
+        .map(|param_info| {
+            let shortname = format_ident!("{}", param_info.shortname);
+            let varname = format_ident!("{}", param_info.varname);
+            quote! { #enumname::#shortname => dsp.#varname = value}
+        })
+        .collect();
+    quote! {
+        impl #enumname {
+            pub fn set(&self, dsp: &mut #dsp_name, value: FaustFloat) {
+                match self {
+                    #(#matches ),*
+                }
+            }
+        }
+    }
+}
+
+pub fn create_passive_impl(infos: &[&ParamInfo], dsp_name: &str) -> TokenStream {
+    let enumname = format_ident!("UIPassiveShortname");
+    let dsp_name = format_ident!("{dsp_name}");
+    let matches: Vec<TokenStream> = infos
+        .iter()
+        .map(|param_info| {
+            let shortname = format_ident!("{}", param_info.shortname);
+            let varname = format_ident!("{}", param_info.varname);
+            quote! { #enumname::#shortname => dsp.#varname}
+        })
+        .collect();
+    quote! {
+        impl #enumname {
+            pub fn get(&self, dsp: & #dsp_name) -> FaustFloat {
+                match self {
+                #(#matches ),*
+                }
+            }
+        }
+    }
+}
+
+pub fn create_enums(v: Vec<ParamInfo>, dsp_name: &str) -> TokenStream {
+    let active: Vec<&ParamInfo> = v.iter().filter(|i| i.is_active).collect();
+    let passive: Vec<&ParamInfo> = v.iter().filter(|i| !i.is_active).collect();
+    let (active_enum, active_impl) = if active.is_empty() {
+        (TokenStream::new(), TokenStream::new())
+    } else {
+        (
+            create_qualified_enum(&active, "Active"),
+            create_active_impl(&active, dsp_name),
+        )
+    };
+    let (passive_enum, passive_impl) = if active.is_empty() {
+        (TokenStream::new(), TokenStream::new())
+    } else {
+        (
+            create_qualified_enum(&passive, "Passive"),
+            create_passive_impl(&passive, dsp_name),
+        )
+    };
+    quote::quote! {
+        #active_enum
+        #active_impl
+        #passive_enum
+        #passive_impl
+    }
 }
