dc0f9134aaefd6ea1b82473807c52a98aa36d32f
crop <crop_tech@proton.me>
2024-12-19 16:52:51 +0100

    use generated enums in example

--- examples/example-f64/build.rs
+++ examples/example-f64/build.rs
@@ -4,67 +4,9 @@ use std::{
 };
 
 use faust_build::FaustBuilder;
-use faust_json::{self, Faust, LayoutItem};
-use quote::quote;
-use tempfile::NamedTempFile;
-
-trait FaustParamEnumBuilder {
-    fn get_isactive_shortname_varname(&self) -> Vec<(bool, String, String)>;
-}
-
-impl FaustParamEnumBuilder for Faust {
-    fn get_isactive_shortname_varname(&self) -> Vec<(bool, String, String)> {
-        self.ui
-            .iter()
-            .flat_map(|items| items.get_isactive_shortname_varname())
-            .collect()
-    }
-}
-
-impl FaustParamEnumBuilder for LayoutItem {
-    fn get_isactive_shortname_varname(&self) -> Vec<(bool, String, String)> {
-        match self {
-            LayoutItem::TGroup { items, .. } => items
-                .iter()
-                .flat_map(|items| items.get_isactive_shortname_varname())
-                .collect(),
-            LayoutItem::VGroup { items, .. } => items
-                .iter()
-                .flat_map(|items| items.get_isactive_shortname_varname())
-                .collect(),
-            LayoutItem::HGroup { items, .. } => items
-                .iter()
-                .flat_map(|items| items.get_isactive_shortname_varname())
-                .collect(),
-            LayoutItem::VSlider {
-                shortname, varname, ..
-            } => vec![(true, shortname.clone(), varname.clone())],
-            LayoutItem::HSlider {
-                shortname, varname, ..
-            } => vec![(true, shortname.clone(), varname.clone())],
-            LayoutItem::NEntry {
-                shortname, varname, ..
-            } => vec![(true, shortname.clone(), varname.clone())],
-            LayoutItem::Button {
-                shortname, varname, ..
-            } => vec![(true, shortname.clone(), varname.clone())],
-            LayoutItem::CheckBox {
-                shortname, varname, ..
-            } => vec![(true, shortname.clone(), varname.clone())],
-            LayoutItem::VBarGraph {
-                shortname, varname, ..
-            } => vec![(false, shortname.clone(), varname.clone())],
-            LayoutItem::HBarGraph {
-                shortname, varname, ..
-            } => vec![(false, shortname.clone(), varname.clone())],
-            LayoutItem::Soundfile {
-                address, varname, ..
-            } => {
-                vec![(true, address.clone(), varname.clone())]
-            }
-        }
-    }
-}
+use faust_json::{self, Faust, GetParmInfo};
+use quote::{format_ident, quote};
+use syn::parse_str;
 
 fn main() {
     println!("cargo:rerun-if-changed=dsp");
@@ -82,101 +24,41 @@ fn main() {
         panic!("{}", err);
     });
 
-    f.get_isactive_shortname_varname();
-    let enum_string = create_enums(f.get_isactive_shortname_varname());
+    let dsp_code: String = b.build_to_stdout();
+    let dsp_code = parse_str::<proc_macro2::TokenStream>(&dsp_code)
+        .expect("Failed to parse string into tokens");
 
-    let template_head = r###"
-    mod <<moduleName>> {
-        #![allow(clippy::all)]
-        #![allow(unused_parens)]
-        #![allow(non_snake_case)]
-        #![allow(non_camel_case_types)]
-        #![allow(dead_code)]
-        #![allow(unused_variables)]
-        #![allow(unused_mut)]
-        #![allow(non_upper_case_globals)]
-        
-        use faust_types::*;
-        <<includeIntrinsic>>
-        <<includeclass>>
+    let module_name = format_ident!("{}", b.module_name.as_str());
+    let struct_name = format_ident!("{}", b.get_struct_name());
+    let parameter_info_enum = faust_json::create_enums(f.get_param_info(), &b.get_struct_name());
 
+    let template = quote! {
+        mod #module_name {
+            #![allow(clippy::all)]
+            #![allow(unused_parens)]
+            #![allow(non_snake_case)]
+            #![allow(non_camel_case_types)]
+            #![allow(dead_code)]
+            #![allow(unused_variables)]
+            #![allow(unused_mut)]
+            #![allow(non_upper_case_globals)]
 
-    "###;
+            use faust_types::*;
 
-    let template_tail = r###"
+            #dsp_code
 
+            #parameter_info_enum
+        }
 
-    }
-    
-    pub use <<moduleName>>::<<structName>>;
-    pub use <<moduleName>>::UIActiveShortname;
-    pub use <<moduleName>>::UIPassiveShortname;
+        pub use #module_name::#struct_name;
+        pub use #module_name::UIActiveShortname;
+        pub use #module_name::UIPassiveShortname;
 
-    "###;
+    };
 
-    let template_code = template_head.to_owned() + &enum_string + template_tail;
-
-    let default_arch = NamedTempFile::new().expect("failed creating temporary file");
-    fs::write(default_arch.path(), template_code).expect("failed writing temporary file");
-    let template_path = default_arch.path().to_str().unwrap().to_owned();
-
-    let b = b.set_arch_file(&template_path);
-
-    b.build();
-}
-
-fn create_qualified_enum<'a>(
-    v: impl Iterator<Item = &'a (bool, String, String)>,
-    q: &str,
-) -> String {
-    let active_lines = v
-        .map(|(_, shortname, _)| shortname.to_owned() + ",")
-        .collect::<String>();
-    let active_enum = "pub enum UI".to_owned() + q + "Shortname {" + &active_lines + "}\n";
-    active_enum
-}
-
-fn create_active_impl<'a>(v: impl Iterator<Item = &'a (bool, String, String)>) -> String {
-    let active_matches = v
-        .map(|(b, shortname, varname)| {
-            "UIActiveShortname::".to_owned() + shortname + "=> dsp." + varname + " = value,\n"
-        })
-        .collect::<String>();
-    let active_impl = "impl UIActiveShortname {".to_owned()
-        + "pub fn set(&self, dsp: &mut Volume, value: FaustFloat) {"
-        + "match self {"
-        + &active_matches
-        + "}"
-        + "}"
-        + "}\n";
-    active_impl
-}
-
-fn create_passive_impl<'a>(v: impl Iterator<Item = &'a (bool, String, String)>) -> String {
-    let active_matches = v
-        .map(|(b, shortname, varname)| {
-            "UIPassiveShortname::".to_owned() + shortname + "=> dsp." + varname + ",\n"
-        })
-        .collect::<String>();
-    let active_impl = "impl UIPassiveShortname {".to_owned()
-        + "pub fn get(&self, dsp: &mut Volume) -> FaustFloat {"
-        + "match self {"
-        + &active_matches
-        + "}"
-        + "}"
-        + "}\n";
-    active_impl
-}
-
-fn create_enums(v: Vec<(bool, String, String)>) -> String {
-    let active_iter = v.iter().filter(|(a, ..)| *a);
-    let passive_iter = v.iter().filter(|(a, ..)| !*a);
-    let active_enum = create_qualified_enum(active_iter.clone(), "Active");
-    let passive_enum = create_qualified_enum(passive_iter.clone(), "Passive");
-    let active_impl = create_active_impl(active_iter.clone());
-    let passive_impl = create_passive_impl(passive_iter.clone());
-    let all = active_enum + &active_impl + &passive_enum + &passive_impl;
-    let t = syn::parse_file(&all).unwrap();
-    let ps = prettyplease::unparse(&t);
-    ps
+    println!("{}", &template.to_string());
+    let parsed: syn::File = syn::parse_file(&template.to_string())
+        .unwrap_or_else(|err| panic!("syn failed with: {}", err.into_compile_error()));
+    let pp = prettyplease::unparse(&parsed);
+    fs::write("src/dsp.rs", pp).expect("failed to write to destination path");
 }
